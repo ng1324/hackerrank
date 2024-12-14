80de3839170e8ab184979469c89150c07e562992
ng1324 <140426664+ng1324@users.noreply.github.com>
2024-12-14 22:47:46 +0800

    Create Equalize the Array.py

---
+++ Prepare/Python solutions/Equalize the Array.py
@@ -0,0 +1,36 @@
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'equalizeArray' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def equalizeArray(arr):
+    dic = {}
+    for item in arr:
+        dic[item] = dic.get(item, 0) + 1
+    a = sorted(dic.items(), key = lambda x:x[1], reverse=True)
+    return len(arr) - a[0][1]
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = equalizeArray(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
